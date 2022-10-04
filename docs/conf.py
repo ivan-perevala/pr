fa5e8ab38e5e94833a0ff06c9548662d061f1077
Ivan Perevala <ivan95perevala@gmail.com>
2022-10-05 01:44:42 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,14 @@ StandaloneHTMLBuilder.supported_image_types = [
     "image/jpeg",
 ]
 
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
 html_theme = "sphinx_rtd_theme"
 html_logo = "bhq_logo_color_v1.svg"
 html_static_path = ["_static"]
@@ -13,5 +21,4 @@ html_templates_path = ["_templates"]
 html_theme_options = {
     "display_version": True,
     "style_external_links": True,
-    "logo_only": True,
 }
